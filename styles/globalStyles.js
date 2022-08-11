8893b01b528efb5315a55895459bdec10563595e
German Rodriguez <germanson@gmail.com>
2022-08-11 12:28:06 -0700

    Adding global styles

---
+++ styles/globalStyles.js
@@ -0,0 +1,14 @@
+// globalStyles.js
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+
+    font-family: 'SF Pro Display';
+    font-size: 14px;
+  }
+`;
+
+export default GlobalStyle;
